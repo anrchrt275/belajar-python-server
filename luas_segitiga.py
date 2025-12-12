bba03653fd2aa960e754c1fc843bdbf7d7b98769
anrchrt275 <riochristian36@gmail.com>
2025-12-12 15:31:46 +0700

    Tambah program luas segitiga

---
+++ luas_segitiga.py
@@ -0,0 +1,6 @@
+print("=== Program Hitung Luas Segitiga ===")
+alas = float(input("Masukkan alas: "))
+tinggi = float(input("Masukkan tinggi: "))
+
+luas = 0.5 * alas * tinggi
+print("Luas segitiga adalah:", luas)
